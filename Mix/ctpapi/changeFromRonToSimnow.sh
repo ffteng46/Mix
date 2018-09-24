73f57e0cfbaf0227666a0034452e509a29ffabe2
ffteng46 <ffteng46@gmail.com>
2018-09-24 06:56:00 -0700

    a

--- Mix/ctpapi/changeFromRonToSimnow.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-sudo mv libthosttraderapi.so libthosttraderapi.so.ron
-sudo mv libthosttraderapi.so.sim libthosttraderapi.so
--- Mix/ctpapi/changeFromSimnowToRon.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-sudo mv libthosttraderapi.so libthosttraderapi.so.sim
-sudo mv libthosttraderapi.so.ron libthosttraderapi.so
